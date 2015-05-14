032380e12498d4c42d51cd94d6403419df31c37f
xxFlare <appletreegold@gmail.com>
2015-05-14 09:56:40 -0400

    Yay classes

--- scr/xFlare/LoadBalancer/Main.php
+++ scr/xFlare/LoadBalancer/Main.php
@@ -1,13 +1,15 @@
 <?php
+namespace xFlare\LoadBalancer;
 
+use pocketmine\event\player\PlayerCommandPreprocessEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\utils\Config;
+use pocketmine\Server;
 
-//Classes
-
-
-//onEnable
-$event->getPlayer()->sendMessage("> This server uses LoadBalancer by xFlare");
-$event->getPlayer()->sendMessage("> You may get redirected to another server.");
-$config = $this->getConfig();
-$number = $config->get("number");
-
-$random_server = rand(0, $number);
+class Main extends PluginBase implements Listener{
+    public function onEnable(){
+        $this->saveDefaultConfig();
+        $config = $this->getConfig();
+        $random_server = rand(0, $number);
+    }
+}
